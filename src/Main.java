9ea4c531ff1ade3cab5e9174cc01a93c10a30b16
unknown <C:\Users\1\AppData\Roaming\The Bat!>
2022-11-11 21:03:42 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Open world");
+        System.out.println("Open world 1");
+        System.out.println("Open world 2");
+        System.out.println("Open world 3");
+    }
+}
